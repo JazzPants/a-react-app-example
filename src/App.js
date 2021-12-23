3e6a20c1d88c9e7a0f12f12d1dcc19b5bc5d7020
JazzPants <leighton.l14@gmail.com>
2021-12-24 02:39:14 +1100

    Done!

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
-import moment from 'moment';
-import ExampleComponent from './ExampleComponent'
-import TestComponent from './TestComponent'
+import React, { Component } from 'react'; //react dependencies - npm packages
+import moment from 'moment'; //moment dependencies - npm packages
+import ExampleComponent from './ExampleComponent' //importing a component
+import TestComponent from './TestComponent' //importing a component
 
 // Add your code own within the return statement
+//{moment().format('MMMM Do YYYY, hh:mm:ss a')}
 
 class App extends Component {
   render() {
@@ -11,7 +12,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          {moment().format('MMMM Do YYYY, hh:mm:ss a')}
+          Now
         </header>
         <p className="App-intro">
           In React apps, we write JSX - it looks like HTML, and uses a lot of HTML syntax.
@@ -19,6 +20,8 @@ class App extends Component {
           allows us to add in components, which are separate, self-contained chunks of JSX.
         </p>
         <ExampleComponent />
+        <ExampleComponent />
+        <TestComponent />
 
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -3,4 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+//render the parent component "App"
 ReactDOM.render(<App />, document.getElementById('root'));
+
+//top of the chain, so don't need to export
